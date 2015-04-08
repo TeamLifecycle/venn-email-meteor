3e7f31468d3902cf2b106607f1c46ae2688a9021
Tim Giblin <timmyg13@gmail.com>
2015-04-08 17:59:33 -0400

    working

--- venn-email.js
+++ venn-email.js
@@ -1,12 +1,17 @@
 vennEmail = Npm.require('venn-email')
 
+
+vennEmail.send2 = function(from, to, subject, msg) {
+	var response = Async.runSync(function(done) {
+	  vennEmail.send(from, to, subject, msg, function(err, result) {
+	  	console.log("result!!!!", result)
+	  	done(err, result)
+	  })
+	});
+	return response.result;
+}
+
+
 vennEmail.initialize("6644bb87bcd7fceb2cd53436")
-
-var response = Async.runSync(function(done) {
-  vennEmail.send("from@email.com", "timmyg13@gmail.com", "sub88tgtg#2", "msg9917", function(err, result) {
-  	console.log("result!!", result)
-  	done(err, result)
-  })
-});
-
-console.log(response.result); // 1001
+var result2 = vennEmail.send2("from@email.com", "timmyg13@gmail.com", "sub88tgtg#6", "msg9917");
+console.log("result2", result2)
